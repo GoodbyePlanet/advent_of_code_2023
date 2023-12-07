459c0da4863424efb955912c54baccdcafd01504
Nemanja <nemanja.vasic@productdock.com>
2023-12-07 22:21:21 +0100

    First day first puzzle

---
+++ day_01/first/main.py
@@ -0,0 +1,25 @@
+def main():
+    with open("input.txt", "r") as file:
+        num_sum = 0
+
+        for line in file:
+            first_num = None
+            last_num = None
+
+            for char in line:
+                if char.isdigit():
+                    first_num = char
+                    break
+
+            for char in line[::-1]:
+                if char.isdigit():
+                    last_num = char
+                    break
+
+            num_sum += int(first_num + last_num)
+
+        print(type(first_num), type(last_num), num_sum)
+
+
+if __name__ == "__main__":
+    main()
