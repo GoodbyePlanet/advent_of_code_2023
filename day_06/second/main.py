716c3eb09b2d9fb2caecf230f4f41ebe4ab6334d
Nemanja <nemanja.vasic@productdock.com>
2023-12-18 16:46:54 +0100

    Day 06 second assignment done

---
+++ day_06/second/main.py
@@ -0,0 +1,30 @@
+from file_utils.read_file import read_file
+
+
+def main(boat_races_input):
+    lines = boat_races_input.splitlines()
+    time = int("".join(lines[0].split("Time:")[1].strip().split()))
+    record_distance = int("".join(lines[1].split("Distance:")[1].strip().split()))
+
+    ways_to_win = []
+    ways = []
+
+    for hold in range(1, time + 1):
+        record_achieved = hold * (time - hold)
+
+        if record_achieved > record_distance:
+            ways.append(hold)
+
+    ways_to_win.append(len(ways))
+
+    mult_of_the_ways = 1
+    for w in ways_to_win:
+        mult_of_the_ways *= w
+
+    return mult_of_the_ways
+
+
+if __name__ == "__main__":
+    boat_races = read_file("day_06_second_input.txt")
+    multiple_of_ways_to_win = main(boat_races)
+    print(multiple_of_ways_to_win)
